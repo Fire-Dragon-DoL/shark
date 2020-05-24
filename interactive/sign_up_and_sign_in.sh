0a270248513bee4bd95d165d0e79cb5225fda277
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2020-05-23 18:22:21 -0700

    Interactive test is now executable

--- interactive/sign_up_and_sign_in.sh
+++ interactive/sign_up_and_sign_in.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# $1 type
+# $2 url
 do_request() {
   curl -o - -f -X "$1" "$2" 2>/dev/null
   curl_result=$?
@@ -10,20 +12,30 @@ do_request() {
   return $curl_result
 }
 
-exercise() {
-  do_request POST 'http://localhost:3000/v1/users'
+# $1 times
+# $2 command
+# $n args
+do_retry() {
+  local args="${@:3}"
+  for run in {1..$1}; do
+    ($2 $args)
+    if [ "$?" -eq 0 ]; then
+      break
+    else
+      sleep 1
+    fi
+  done
 }
 
+result=1
 docker-compose build
 docker-compose up -d --force-recreate --remove-orphans
-for run in {1..5}; do
-  if exercise; then
-    break
-  else
-    sleep 1
-  fi
-done
-result=$?
+
+if do_retry 4 do_request POST 'http://localhost:3000/v1/users'; then
+  # sign in
+  result=0
+fi
+
 docker-compose down
 
 if [ "$result" -eq 0 ]; then
