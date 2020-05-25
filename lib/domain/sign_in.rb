aa56ccc28db3ab68dc790d9fc19bb8808464faca
Fire-Dragon-DoL <francesco.belladonna@gmail.com>
2020-05-25 15:58:27 -0700

    User can now sign in

---
+++ lib/domain/sign_in.rb
@@ -0,0 +1,32 @@
+require 'domain/password'
+require 'domain/user/get'
+require 'securerandom'
+
+module Domain
+  class SignIn
+    attr_accessor :get
+    attr_accessor :match
+
+    def initialize
+      @get ||= User::Get.new
+      @match ||= Password::Match.new
+    end
+
+    def call(username, password)
+      digested_pass = get.(username)
+
+      return nil if digested_pass.nil?
+      return nil unless match.(digested_pass, password)
+      self.class.build_token
+    end
+
+    def self.call(username, password)
+      instance = new
+      instance.(username, password)
+    end
+
+    def self.build_token
+      SecureRandom.urlsafe_base64(32)
+    end
+  end
+end
---
+++ test/lib/domain/sign_in_test.rb
@@ -0,0 +1,44 @@
+require 'test_helper'
+require 'domain/user/get'
+require 'domain/sign_in'
+require 'domain/username'
+require 'domain/password'
+
+module Domain
+  class SignInTest < ActiveSupport::TestCase
+    test "With existing username and matching password is successful" do
+      username = Username::Sample.random
+      password = Password::Sample.default
+
+      sign_in = SignIn.new
+      sign_in.get = User::Get::Substitute.new(username, password)
+      token = sign_in.(username, password)
+
+      refute token.nil?
+    end
+
+    test "With missing username fails" do
+      username = Username::Sample.random
+      password = Password::Sample.default
+      other_username = Username::Sample.default
+
+      sign_in = SignIn.new
+      sign_in.get = User::Get::Substitute.new(other_username, password)
+      token = sign_in.(username, password)
+
+      assert token.nil?
+    end
+
+    test "With existing username and incorrect password fails" do
+      username = Username::Sample.random
+      password = Password::Sample.default
+      other_password = Password::Sample.random
+
+      sign_in = SignIn.new
+      sign_in.get = User::Get::Substitute.new(username, password)
+      token = sign_in.(username, other_password)
+
+      assert token.nil?
+    end
+  end
+end
